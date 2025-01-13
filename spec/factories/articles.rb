d1512b0ea4b6bfa5e02b89d46f77fea0cd735e10
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2025-01-13 10:51:20 -0300

    Added traits feature

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
         title { "Sample Article Title" }
         body { "This is a sample article content." }
         status { "public" }
+
+        trait :private do
+            status { "private" }
+        end
+
+        trait :archived do
+            status { "archived" }
+        end
     end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   context 'create' do
     article = FactoryBot.create(:article)
+
     it 'is valid with valid attributes' do
       expect(article).to be_valid
     end
@@ -10,6 +11,24 @@ RSpec.describe Article, type: :model do
     it 'is not valid without a title' do
       article.title = nil
       expect(article).to_not be_valid
+    end
+  end
+
+  context 'visibility' do
+    context 'private' do
+      article = FactoryBot.create(:article, :private)
+
+      it 'is private' do
+        expect(article.status).to eq("private")
+      end
+    end
+
+    context 'archived' do
+      article = FactoryBot.create(:article, :archived)
+
+      it 'is archived' do
+        expect(article.status).to eq("archived")
+      end
     end
   end
 end
