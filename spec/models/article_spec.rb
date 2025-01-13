7602cda55a86dddb927367ea279eb5a63edd249d
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2025-01-13 10:57:55 -0300

    Enhanced FactoryBot usage by: - Replacing `create` with `build_stubbed` - Replacing variable usage with `let`

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   context 'create' do
-    article = FactoryBot.build_stubbed(:article)
+    let(:article) { FactoryBot.build_stubbed(:article) }
 
     it 'is valid with valid attributes' do
       expect(article).to be_valid
@@ -16,7 +16,7 @@ RSpec.describe Article, type: :model do
 
   context 'visibility' do
     context 'private' do
-      article = FactoryBot.build_stubbed(:article, :private)
+      let(:article) { FactoryBot.build_stubbed(:article, :private) }
 
       it 'is private' do
         expect(article.status).to eq("private")
@@ -24,7 +24,7 @@ RSpec.describe Article, type: :model do
     end
 
     context 'archived' do
-      article = FactoryBot.build_stubbed(:article, :archived)
+      let(:article) { FactoryBot.build_stubbed(:article, :archived) }
 
       it 'is archived' do
         expect(article.status).to eq("archived")
